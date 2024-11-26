2e4da4d75ac806718dc1cc7c77486dbfc740223a
dxninob <60080916+dxninob@users.noreply.github.com>
2024-11-26 00:01:13 +0000

    :hammer: Updated split

--- src/3_train_test_split.py
+++ src/3_train_test_split.py
@@ -1,26 +1,27 @@
 import pandas as pd
-import numpy as np
 import sys
 import os
-import seaborn as sns
-import matplotlib.pyplot as plt
+
+
 
 def load_datasets():
     """Load all datasets and return them as dataframes."""
     current_dir = os.getcwd()
     ROOT_PATH = os.path.dirname(current_dir)
     sys.path.insert(1, ROOT_PATH)
+    sys.path.insert(1, current_dir)
     import root
 
     datos = pd.read_pickle(root.DIR_DATA_STAGE + 'merged_df.pkl')
-    
     return  datos
 
+
 def train_test_split(datos, fin_train, ):
+    """Split dataset into train and test."""
     datos_train = datos.loc[: fin_train, :]
     datos_test  = datos.loc[fin_train:, :]
-
     return datos_train, datos_test
+
 
 def save_datasets_to_pickle(datasets, paths=None):
     """Save each dataset in datasets list to the corresponding path in paths list as a pickle file."""
@@ -39,17 +40,18 @@ def save_datasets_to_pickle(datasets, paths=None):
     for dataset, path in zip(datasets, paths):
         dataset.to_pickle(path)
 
+
 def main():
     # Read datasets
     datos = load_datasets()
     
-    # Prepare date columns
+    # Split dataset
     fin_train = '2023-01-31 23:59:00'
-    datos_train, datos_test = train_test_split(datos, fin_train)
-    
+    datos_train, datos_test = train_test_split(datos, fin_train)    
+
     # Save datasets
     save_datasets_to_pickle([datos_train, datos_test])
 
+
 if __name__ == "__main__": 
     main()
-
